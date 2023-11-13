8e587f4a9d102dc3055c348f86fb4c719a671232
Zachary Goulet <zachary.goulet9@gmail.com>
2023-11-13 18:14:25 -0500

    progress

--- examples/post.rs
+++ examples/post.rs
@@ -1,6 +1,4 @@
-use std::f32::consts::PI;
 use bevy::prelude::*;
-
 
 //noinspection DuplicatedCode
 
@@ -8,23 +6,24 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_scene)
+        .add_systems(Update, move_cube_to_the_right)
         .run();
 }
 
+#[derive(Component)]
+struct Cube;
 
 fn spawn_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-
     // Spawn platform for reference
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane::from_size(40.0))),
         material: materials.add(Color::SEA_GREEN.into()),
         ..default()
     });
-
 
     // Spawn Camera
     let player_camera_y_offset: f32 = 20.0;
@@ -36,19 +35,25 @@ fn spawn_scene(
         ..default()
     });
 
-
     // Add global light
     commands.insert_resource(AmbientLight {
         color: Default::default(),
         brightness: 1.0,
     });
 
+    // Spawn cube
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
+            material: materials.add(Color::BEIGE.into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        })
+        .insert(Cube);
+}
 
-    // Spawn object
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
-        material: materials.add(Color::BEIGE.into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
+fn move_cube_to_the_right(mut query: Query<&mut Transform, With<Cube>>) {
+    let mut transform = query.single_mut();
+
+    transform.translation.x += 0.05;
 }
