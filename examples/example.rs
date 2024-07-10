4035273b9feac9c0b91b0ae52b0ae8e004df74bd
zacharygoulet <zachary.goulet9@gmail.com>
2024-07-09 21:27:12 -0400

    update to bevy 14

--- examples/example.rs
+++ examples/example.rs
@@ -1,14 +1,19 @@
-use bevy::prelude::*;
+use bevy::{
+    color::palettes::css::{BEIGE, RED, SEA_GREEN},
+    prelude::*,
+};
+use bevy_destination_svn::destination::{
+    Destination, DestinationBundle, DestinationPlugin, DestinationSpeed,
+};
 use bevy_rapier3d::prelude::*;
-use bevy_destination_svn::destination::{Destination, DestinationBundle, DestinationPlugin, DestinationSpeed};
 
 fn main() {
-    App::new().
-        add_plugins((
+    App::new()
+        .add_plugins((
             DefaultPlugins,
             RapierPhysicsPlugin::<NoUserData>::default(),
-            DestinationPlugin
-    ))
+            DestinationPlugin,
+        ))
         .add_systems(Startup, spawn_scene)
         .add_systems(Update, set_next_destination)
         .add_systems(Update, update_speed)
@@ -17,27 +22,25 @@ fn main() {
         .run();
 }
 
-
 #[derive(Component)]
 struct DestinationMarker;
-
 
 fn spawn_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-
     let starting_point = Vec3::new(0.0, 0.5, 0.0);
     let first_destination = Vec3::new(5.0, 0.5, 5.0);
 
     // Spawn object
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(Color::BEIGE),
-        transform: Transform::from_translation(starting_point),
-        ..default()
-    })
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(Color::Srgba(BEIGE)),
+            transform: Transform::from_translation(starting_point),
+            ..default()
+        })
         .insert(RigidBody::KinematicVelocityBased)
         .insert(Velocity::default())
         .insert(DestinationBundle {
@@ -46,13 +49,14 @@ fn spawn_scene(
         });
 
     // Spawn destination marker
-    commands.spawn( PbrBundle {
-        mesh: meshes.add(Sphere::new(0.4).mesh().ico(5).unwrap()),
-        material: materials.add(Color::RED),
-        transform: Transform::from_translation(first_destination),
-        ..default()
-    }).insert(DestinationMarker);
-
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Sphere::new(0.4).mesh().ico(5).unwrap()),
+            material: materials.add(Color::Srgba(RED)),
+            transform: Transform::from_translation(first_destination),
+            ..default()
+        })
+        .insert(DestinationMarker);
 
     let player_camera_y_offset: f32 = 25.0;
     let player_camera_z_offset: f32 = 10.0;
@@ -67,37 +71,31 @@ fn spawn_scene(
     // Spawn platform
     commands.spawn(PbrBundle {
         mesh: meshes.add(Plane3d::default().mesh().size(30.0, 30.0)),
-        material: materials.add(Color::SEA_GREEN),
+        material: materials.add(Color::Srgba(SEA_GREEN)),
         ..default()
     });
-
 
     // Add global light
     commands.insert_resource(AmbientLight {
         color: Default::default(),
         brightness: 1000.0,
     });
-
 }
 
 fn set_next_destination(
     mut query: Query<(&mut Destination, &Transform)>,
-    mut destination_marker: Query<&mut Transform, (With<DestinationMarker>, Without<Destination>)>
+    mut destination_marker: Query<&mut Transform, (With<DestinationMarker>, Without<Destination>)>,
 ) {
-
-
     query.iter_mut().for_each(|(mut destination, transform)| {
         match &mut *destination {
-
             Destination::Target(_) => {}
 
             // set new destination when reached previous
             Destination::Reached => {
-
                 let next_destination = Vec3::new(
                     rand::random::<f32>() * 20.0 - 10.0,
                     0.5,
-                    rand::random::<f32>() * 20.0 - 10.0
+                    rand::random::<f32>() * 20.0 - 10.0,
                 );
 
                 *destination = Destination::new(transform.translation, next_destination);
@@ -107,34 +105,37 @@ fn set_next_destination(
             }
         }
     });
-
 }
-
 
 fn update_speed(
     keyboard_inputs: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut DestinationSpeed>
+    mut query: Query<&mut DestinationSpeed>,
 ) {
     query.iter_mut().for_each(|mut speed| {
-        if keyboard_inputs.just_pressed(KeyCode::ArrowUp) { speed.translation += 1.0; }
-        if keyboard_inputs.just_pressed(KeyCode::ArrowDown) { speed.translation -= 1.0; }
+        if keyboard_inputs.just_pressed(KeyCode::ArrowUp) {
+            speed.translation += 1.0;
+        }
+        if keyboard_inputs.just_pressed(KeyCode::ArrowDown) {
+            speed.translation -= 1.0;
+        }
     });
 }
 
 fn update_rotation_speed(
     keyboard_inputs: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut DestinationSpeed>
+    mut query: Query<&mut DestinationSpeed>,
 ) {
     query.iter_mut().for_each(|mut rotation_speed| {
-        if keyboard_inputs.just_pressed(KeyCode::ArrowRight) { rotation_speed.rotation += 2.0; }
-        if keyboard_inputs.just_pressed(KeyCode::ArrowLeft) { rotation_speed.rotation -= 2.0; }
+        if keyboard_inputs.just_pressed(KeyCode::ArrowRight) {
+            rotation_speed.rotation += 2.0;
+        }
+        if keyboard_inputs.just_pressed(KeyCode::ArrowLeft) {
+            rotation_speed.rotation -= 2.0;
+        }
     });
 }
 
-fn update_time_speed(
-    keyboard_inputs: Res<ButtonInput<KeyCode>>,
-    mut time: ResMut<Time<Virtual>>
-) {
+fn update_time_speed(keyboard_inputs: Res<ButtonInput<KeyCode>>, mut time: ResMut<Time<Virtual>>) {
     if keyboard_inputs.just_pressed(KeyCode::KeyG) {
         let new_speed = time.relative_speed() - 0.2;
         time.set_relative_speed(new_speed);
